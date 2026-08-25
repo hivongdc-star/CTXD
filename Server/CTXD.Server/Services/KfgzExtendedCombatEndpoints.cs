a2a673ea7e945308131bdc75585575af8ef42f5c
hivongdc-star <hivongdc@gmail.com>
2026-08-25 23:52:52 +0900

    Start KFGZ mubing worker with endpoint module

--- Server/CTXD.Server/Services/KfgzExtendedCombatEndpoints.cs
+++ Server/CTXD.Server/Services/KfgzExtendedCombatEndpoints.cs
@@ -6,6 +6,8 @@ public static class KfgzExtendedCombatEndpoints
 {
     public static IEndpointRouteBuilder MapKfgzExtendedCombat(this IEndpointRouteBuilder app)
     {
+        KfgzMubingWorker.Start(app.ServiceProvider);
+
         app.MapGet("/api/kfgz/resources",async(HttpRequest request,AuthService auth,KfgzExtendedCombatService combat,GameDb db,CanonicalContent content,ResourceProductionService production,TechnologyEffectService technologies,GamePushHub push,CancellationToken ct)=>
         {
             var id=await auth.ResolvePlayerIdAsync(Bearer(request),ct);
