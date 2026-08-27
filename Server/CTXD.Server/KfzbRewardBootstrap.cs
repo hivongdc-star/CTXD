6e07ad14d0c222eae54c2c4434a7457d3a15b9fd
hivongdc-star <hivongdc@gmail.com>
2026-08-27 20:38:48 +0900

    Register KFZB reward routes and General Treasure service

---
+++ Server/CTXD.Server/KfzbRewardBootstrap.cs
@@ -0,0 +1,25 @@
+using Microsoft.Extensions.DependencyInjection;
+
+public static class WebApplication
+{
+    public static Microsoft.AspNetCore.Builder.WebApplicationBuilder CreateBuilder(string[] args)
+    {
+        var builder=Microsoft.AspNetCore.Builder.WebApplication.CreateBuilder(args);
+        builder.Services.AddSingleton<CTXD.Server.Services.GeneralTreasureService>();
+        return builder;
+    }
+}
+
+namespace CTXD.Server.Services
+{
+    public static class KfzbRewardRouteBootstrap
+    {
+        public static Microsoft.AspNetCore.Builder.WebApplication MapKfgzExtendedCombat(this Microsoft.AspNetCore.Builder.WebApplication app)
+        {
+            KfgzExtendedCombatEndpoints.MapKfgzExtendedCombat((Microsoft.AspNetCore.Routing.IEndpointRouteBuilder)app);
+            app.MapKfzbRewardEndpoints();
+            app.MapGeneralTreasureEndpoints();
+            return app;
+        }
+    }
+}
